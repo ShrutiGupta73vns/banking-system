8efafb7cc77d9ad76f33800edac6d44853b10005
Shruti Gupta <shrurose73@gmail.com>
2023-11-20 20:11:23 +0530

    some changes made

--- client/src/pages/Transaction.jsx
+++ client/src/pages/Transaction.jsx
@@ -1,17 +1,16 @@
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useEffect, useState } from 'react';
-
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-     backgroundColor: theme.palette.common.black,
+    backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
@@ -20,58 +19,57 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-
-
 export default function Transaction() {
+  const [transactionData, setTransactionData] = useState([]);
 
-const [transactionData, setTransactionData] = useState([])
+  useEffect(() => {
+    fetch("api/v1/transactions/", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => setTransactionData(data));
+  }, []);
 
-  useEffect(()=>{
-     fetch('api/v1/transactions/',{method: 'GET'})
-     .then(res => res.json())
-     .then(data => setTransactionData(data))
-  },[])
-
-console.log(transactionData)
+  console.log(transactionData);
 
   return (
     <div className="max-w-3xl mx-auto mt-32">
-        <h1 className='text-3xl font-bold text-center mb-9 text-slate-500'>Transactions</h1>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Sender</StyledTableCell>
-            <StyledTableCell>Reciever</StyledTableCell>
-            <StyledTableCell>Amount</StyledTableCell>
-            <StyledTableCell >TimeStamp</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {transactionData.map((row) => (
-            <StyledTableRow key={row._id}>
-              <StyledTableCell component="th" scope="row">
-                {row.sender}
-              </StyledTableCell>
-              <StyledTableCell >{row.reciever}</StyledTableCell>
-              <StyledTableCell >{row.amount}</StyledTableCell>
-              <StyledTableCell >{row.timestamp.substring(0, 10)}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-
-    
+      <h1 className="text-3xl font-bold text-center mb-9 text-slate-500">
+        Transactions
+      </h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Sender</StyledTableCell>
+              <StyledTableCell>Reciever</StyledTableCell>
+              <StyledTableCell>Amount</StyledTableCell>
+              <StyledTableCell>TimeStamp</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transactionData.map((row) => (
+              <StyledTableRow key={row._id}>
+                <StyledTableCell component="th" scope="row">
+                  {row.sender}
+                </StyledTableCell>
+                <StyledTableCell>{row.reciever}</StyledTableCell>
+                <StyledTableCell>{row.amount}</StyledTableCell>
+                <StyledTableCell>
+                  {row.timestamp.substring(0, 10)}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
--- client/src/pages/Transfer.jsx
+++ client/src/pages/Transfer.jsx
@@ -1,7 +1,6 @@
 import { Button } from "@radix-ui/themes";
 import { useState } from "react";
-import {  useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 export default function Transfer() {
   const [formData, setFormData] = useState({
@@ -24,7 +23,7 @@ export default function Transfer() {
 
   const handleSubmit = async (event) => {
     try {
-      setLoading(true)
+      setLoading(true);
       event.preventDefault();
       const res = await fetch("api/v1/user/", {
         method: "PATCH",
@@ -42,7 +41,7 @@ export default function Transfer() {
       }
       setLoading(false);
       setError(null);
-      navigate('/transaction')
+      navigate("/transaction");
     } catch (error) {
       setLoading(false);
       setError(error.message);
@@ -50,43 +49,41 @@ export default function Transfer() {
   };
   return (
     <div className="bg-cyan-100 h-screen  pt-4 pb-18 m-0">
+      <div className="max-w-lg mx-auto mt-32 p-8 border-2 border-cyan-200 rounded-lg shadow-xl bg-white">
+        <h1 className="text-3xl font-bold text-center text-slate-700 my-12">
+          Transfer Money
+        </h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            onChange={handleChange}
+            className="p-3 border-2  border-slate-400 rounded-lg"
+            type="text"
+            placeholder="Senders Name"
+            id="from"
+          />
+          <input
+            onChange={handleChange}
+            className="p-3 border-2   border-slate-400 rounded-lg"
+            type="text"
+            placeholder="Recievers Name"
+            id="to"
+          />
+          <input
+            onChange={handleChange}
+            className="p-3 border-2 border-slate-400 rounded-lg"
+            type="number"
+            placeholder="Amount"
+            id="amount"
+          />
+          <Button disabled={loading} radius="large" variant="soft">
+            Send
+          </Button>
+        </form>
 
-    <div className="max-w-lg mx-auto mt-32 p-8 border-2 border-cyan-200 rounded-lg shadow-xl bg-white">
-      <h1 className="text-3xl font-bold text-center text-slate-700 my-12">Transfer Money</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          onChange={handleChange}
-          className="p-3 border-2  border-slate-400 rounded-lg"
-          type="text"
-          placeholder="Senders Name"
-          id="from"
-        />
-        <input
-          onChange={handleChange}
-          className="p-3 border-2   border-slate-400 rounded-lg"
-          type="text"
-          placeholder="Recievers Name"
-          id="to"
-        />
-        <input
-          onChange={handleChange}
-          className="p-3 border-2 border-slate-400 rounded-lg"
-          type="number"
-          placeholder="Amount"
-          id="amount"
-        />
-        <Button disabled={loading} radius="large" variant="soft">
-          Send
-        </Button>
-      </form>
-
-      <div className="">
-        {error && <p className="text-red-700 font-bold my-5">{error}</p>}
+        <div className="">
+          {error && <p className="text-red-700 font-bold my-5">{error}</p>}
+        </div>
       </div>
-
-      
-    </div>
-    
     </div>
   );
 }
